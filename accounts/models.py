8bc65b0eed73da39987665b11cbdf10a3a063131
aly <pymalien@gmail.com>
2023-04-02 15:13:43 +0200

    base de donnes

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,28 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('vous devez entrer un email')
+
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs["is_staff"] = True
+        kwargs["is_admin"] = True
+        kwargs["is_superuser"] = True
 
 
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(
         unique=True,
-        max_length=5,
+        max_length=255,
         blank=True
     )
     is_active = models.BooleanField(default=True)
@@ -15,6 +32,7 @@ class CustomUser(AbstractBaseUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     USERNAME = "email"
+    objects = MyUserManager()
 
     def has_perm(self, perm, obj=None):
         return True
